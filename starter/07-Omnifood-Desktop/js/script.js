76905dd31bda5bdc66d963daddf69e7331189e3f
Ben Schultze <ben_schultze@outlook.com>
2021-09-30 19:00:57 -0400

    commit

--- starter/07-Omnifood-Desktop/js/script.js
+++ starter/07-Omnifood-Desktop/js/script.js
@@ -1,3 +1,20 @@
+console.log("Hello World!");
+
+const myName = "Benjamin Schultze";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+// Automatically updates the year in HTML copyright in Footer
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
